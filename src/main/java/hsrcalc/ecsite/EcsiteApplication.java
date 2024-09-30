9111ebe1518db5c372d740b87520ae8aa8f621e7
namskram <wildbiduuf@gmail.com>
2024-09-30 11:00:17 -0700

    Set up Spring Boot for Maven Project

---
+++ src/main/java/hsrcalc/ecsite/EcsiteApplication.java
@@ -0,0 +1,13 @@
+package hsrcalc.ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsiteApplication.class, args);
+	}
+
+}
