acd3139fae2da031ad7b26fd76409ddd89d37a58
namskram <wildbiduuf@gmail.com>
2024-09-30 14:46:18 -0700

    created basis for website

---
+++ src/main/java/hsrcalc/ecsite/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package hsrcalc.ecsite.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "index"; // returns the view or data
+    }
+}
